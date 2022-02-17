2a6b4f5380b9bf88f15af01fe898af38d268c559
Magnitopic <magnitopic@gmail.com>
2022-02-18 00:59:03 +0100

    New advancements

---
+++ components/Second.js
@@ -0,0 +1,24 @@
+import styles from "../styles/Second.module.css";
+
+function Second() {
+	return (
+		<div id={styles.main}>
+			<p id={styles.titulo}>Que es CoderDojo?</p>
+			<p id={styles.text}>
+				CoderDojo es una iniciativa sin ánimo de lucro nacida en Irlanda
+				en 2011, que promueve el despliegue de clubes de programación,
+				donde los ninjas (niños y niñas, chicas y chicos de entre 7 y 17
+				años), aprenden a programar en un entorno abierto e informal, de
+				acuerdo a sus propios intereses, y siguiendo su ritmo de
+				aprendizaje. Los clubes son organizados y gestionados por
+				voluntarios y voluntarias (conocidos como champions), y cuentan
+				con la participación de mentores, personas con conocimientos de
+				lenguajes y herramientas de programación, que desinteresadamente
+				dedican parte de su tiempo libre para guiar a los ninjas en el
+				aprendizaje de las diferentes disciplinas, y lenguajes.
+			</p>
+		</div>
+	);
+}
+
+export default Second;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Header from '../components/Header';
 import First from '../components/First';
+import Second from '../components/Second';
 
 export default function Home() {
   return (
     <>
       <Header />
       <First />
+      <Second />
     </>
   )
 }
