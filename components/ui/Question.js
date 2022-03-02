451361ad3cb0b080bd34aa2ace6a6c87e1e95307
Magnitopic <magnitopic@gmail.com>
2022-03-02 11:00:01 +0100

    Fixed deployment bug

--- components/ui/Question.js
+++ components/ui/Question.js
@@ -2,15 +2,13 @@ import styles from "../../styles/Question.module.css";
 import { useState } from "react";
 
 const Question = ({ text }) => {
-	const [position, setPosition] = useState("0");
-
 	return (
 		<div className={styles.question}>
 			<div id={styles.title}>
 				{text}
 				<button id={styles.button}>&#709;</button>
 			</div>
-			<div id={styles.text}>No, no you don't</div>
+			<div id={styles.text}>No, no you don&apos;t</div>
 		</div>
 	);
 };
