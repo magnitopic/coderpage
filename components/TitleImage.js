6f3aa4ed068a763a51324187cff5555473f11dd3
Magnitopic <magnitopic@gmail.com>
2022-03-16 23:50:19 +0100

    New random image

--- components/TitleImage.js
+++ components/TitleImage.js
@@ -1,11 +1,40 @@
-import styles from "../styles/TitleImage.module.css";
-
 
 const TitleImage = () => {
+	// Background Image script
+	
+
 	return (
-		<div id={styles.image}>
-			<div id={styles.imageText} className="title">CoderDojo Majadahonda</div>
-		</div>
+		<>
+			<style jsx>{`
+				#image {
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					text-align: center;
+					background-image: url(/dojo${Math.round(Math.random() * 1)}.jpg);
+					width: 100%;
+					height: 35em;
+					background-position: center;
+					background-repeat: no-repeat;
+					background-size: cover;
+				}
+
+				#imageText {
+					padding: 10px 20px 10px 10px;
+					font-size: 4em;
+					width: fit-content;
+					background-color: rgb(0, 0, 0); /* Fallback color */
+					background-color: rgba(0, 0, 0, 0.7);
+					color: white;
+					z-index: 2;
+				}
+			`}</style>
+			<div id="image">
+				<div id="imageText" className="title">
+					CoderDojo Majadahonda
+				</div>
+			</div>
+		</>
 	);
 };
 
