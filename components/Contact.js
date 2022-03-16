fb93b67897a234929954e601c113663e7cec376c
Magnitopic <magnitopic@gmail.com>
2022-03-16 23:29:50 +0100

    DeployFix

--- components/Contact.js
+++ components/Contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
 					<iframe
 						id={styles.map}
 						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3035.038259984537!2d-3.86855358418593!3d40.474418579358506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd418489f366583f%3A0xe6ed3ae6ca81ad8!2sCentro%20Juvenil%20Pr%C3%ADncipe%20de%20Asturias!5e0!3m2!1ses!2ses!4v1647441643382!5m2!1ses!2ses"
-						allowfullscreen=""
+						allowFullScreen=""
 						loading="lazy"
 					></iframe>
 					<div id={styles.list}>
