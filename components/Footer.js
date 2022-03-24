dbd36fbb3e3dccd7786cc68445b83dc9b0da2abc
magnitopic <magnitopic@gmail.com>
2022-03-24 19:57:51 +0100

    Cambios del Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,23 +1,27 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Footer.module.css";
 import Logo from "./ui/Logo";
 
 const Footer = () => {
 	return (
 		<div id={styles.main}>
-			<Logo id={styles.logo}/>© All rights reserved 2021
-			<div id={styles.socials}>
-				<div className={styles.links}>
-					<div className={styles.logos}>
-						<Logo />
-						<Logo />
-						<Logo />
-					</div>
-					<div id={styles.socialLinks}>
+			<div><Logo /></div>
+			<p>© All rights reserved 2021</p>
+			<div className={styles.socials}>
+				<Link href="https://twitter.com/codernauts_es" passHref>
+					<div className={styles.link}>
+						<a className="fa fa-twitter" id={styles.twitter}/>
 						<p>Twitter</p>
-						<p>YouTube</p>
-						<p>GitHub</p>
 					</div>
+				</Link>
+				<div className={styles.link}>
+					<a className="fa fa-youtube-play" id={styles.youtube}/>
+					<p>YouTube</p>
+				</div>
+				<div className={styles.link}>
+					<a className="fa fa-github" id={styles.github}/>
+					<p>GitHub</p>
 				</div>
 			</div>
 		</div>
